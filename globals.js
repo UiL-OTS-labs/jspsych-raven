fdd0ad7d1e76d56d3913cf80e1c88ba47a112c7e
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2023-08-15 09:21:41 +0200

    Add answers

--- globals.js
+++ globals.js
@@ -5,3 +5,18 @@ const ACCESS_KEY = '00000000-0000-0000-0000-000000000000';
 // max duration of the debriefing message.
 const DEBRIEF_MESSAGE_DURATION = 30000;
 
+
+const REDIRECTION_PAGE = "http://www.nos.nl";
+
+const ANSWERS = [
+    // A
+    4,5,1,2,6,3,6,2,1,3,4,5,
+    // B
+    2,6,1,2,1,3,5,6,4,3,4,5,
+    // C
+    8,2,3,8,7,4,5,1,7,6,1,2,
+    // D
+    3,4,3,7,8,6,5,4,1,2,5,6,
+    // E
+    7,6,8,2,1,5,1,6,3,2,4,5
+];
